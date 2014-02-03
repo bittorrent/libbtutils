933dff360d703710dc67779a1f1e53682d1d750d
Robert McDonald <rmcdonald@bittorrent.com>
2014-02-03 09:45:28 -0800

    Plug memory leak in get_arpa test, make constant static variables const

--- unittests/TestSockAddr.cpp
+++ unittests/TestSockAddr.cpp
@@ -32,7 +32,7 @@ enum AddrType
 	AT_ANY = 2
 };
 
-static std::vector<const char*> ip_cstr
+static const std::vector<const char*> ip_cstr
 {
     "0.255.0.255",
     "255.0.255.0",
@@ -43,7 +43,7 @@ static std::vector<const char*> ip_cstr
     "0.0.0.255"
 };
 
-static std::vector <const char*> return_chars
+static const std::vector <const char*> return_chars
 {
     "255.0.255.0.in-addr.arpa",
     "0.255.0.255.in-addr.arpa",
@@ -95,7 +95,9 @@ TEST(SockAddr, get_arpa)
         }
         //std::cout << buf << std::endl;
         match_strings(ip_cstr[i], buf);
-        EXPECT_STREQ(return_chars[i], sockaddr.get_arpa());
+        char * arpa = sockaddr.get_arpa();
+        EXPECT_STREQ(return_chars[i], arpa);
+        free(arpa);
     }
 }
 
