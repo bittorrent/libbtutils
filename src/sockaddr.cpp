8d229fed9ee34a9d3e54e0a862256b8ad10a1e94
arvidn <arvid@cs.umu.se>
2014-02-06 11:27:15 -0800

    add inet_pton for windows XP, since the OS doesn't provide it

--- src/sockaddr.cpp
+++ src/sockaddr.cpp
@@ -225,6 +225,47 @@ uint32 parse_ip(cstr ip, bool *valid)
 	return r;
 }
 
+#if defined _WIN32 && _WIN32_WINNT < 0x600
+// This function didn't appear in windows until vista.
+// so we need to defined it ourselves here
+int inet_pton(int af, const char* src, void* dest)
+{
+	if (af != AF_INET && af != AF_INET6)
+	{
+		return -1;
+	}
+
+	SOCKADDR_STORAGE address;
+	int address_length = sizeof(SOCKADDR_STORAGE);
+	int result = WSAStringToAddressA((char*)(src), af, 0,
+									 (sockaddr*)(&address),
+									 &address_length);
+
+	if (af == AF_INET)
+	{
+		if (result != SOCKET_ERROR)
+		{
+			sockaddr_in* ipv4_address =(sockaddr_in*)(&address);
+			memcpy(dest, &ipv4_address->sin_addr, sizeof(in_addr));
+		}
+		else if (strcmp(src, "255.255.255.255") == 0)
+		{
+			((in_addr*)(dest))->s_addr = INADDR_NONE;
+		}
+	}
+	else // AF_INET6
+	{
+		if (result != SOCKET_ERROR)
+		{
+			sockaddr_in6* ipv6_address = (sockaddr_in6*)(&address);
+			memcpy(dest, &ipv6_address->sin6_addr, sizeof(in6_addr));
+		}
+	}
+
+	return result == SOCKET_ERROR ? -1 : 1;
+}
+#endif
+
 in6_addr parse_ip_v6(cstr ip_v6, bool *valid)
 {
 	in6_addr a = IN6ADDR_ANY_INIT;
